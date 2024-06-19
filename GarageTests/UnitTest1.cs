ab2a0db41c91a5a8a4fe11c743aa0fc52ca7ed1e
Karsten Tvarnö <vvarrior@gmail.com>
2024-06-19 10:00:58 +0200

    Added tests for methods in Garage class

--- GarageTests/UnitTest1.cs
+++ GarageTests/UnitTest1.cs
@@ -1,13 +1,58 @@
-using Ovning5;
+using Ovning5.Garage;
+using Ovning5.Vehicle;
+using V = Ovning5.Vehicle;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata;
 
 namespace GarageTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void TestGarage()
+        public Garage<Vehicle> garage;
+        public V.Airplane vehicle;
+
+        public UnitTest1()
         {
-            //Assert.
+            garage = new Garage<Vehicle>(10);
+            vehicle = new V.Airplane("ABC123", "RED", 3, 2);
+            garage.AddVehicle(vehicle);
+        }
+        [Fact]
+        public void TestGarageSize()
+        {
+            int expected = 10;
+
+            int actual = garage.GarageSize();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestVehicleAdd()
+        {
+            int expected = 1;
+
+            int actual = garage.AddVehicle(vehicle);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestFindVehicle()
+        {
+            int expected = 0;
+
+            int actual = garage.FindVehicle(vehicle);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestRemoveVehicle()
+        {
+            garage.RemoveVehicle(0);
+
+            Assert.Empty(garage);
         }
     }
 }
